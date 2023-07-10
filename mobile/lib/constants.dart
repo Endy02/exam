b8d1a2b6d97f7411c5271f09433d9694a7990a75
Endy <endy.alexis@outlook.com>
2023-07-10 14:17:48 +0200

    Update flutter app

---
+++ mobile/lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const cuteGreen = Color.fromRGBO(160, 204, 131, 1);
+const weezySand = Color.fromRGBO(242, 208, 109, 1);
+const creamOrange = Color.fromRGBO(242, 175, 94, 1);
+const dangerRed = Color.fromRGBO(216, 98, 71, 1);
+const sweatViolet = Color.fromRGBO(157, 130, 216, 1);
+const funBlue = Color.fromRGBO(106, 216, 205, 1);
+const darkGrey = Color.fromRGBO(126, 124, 124, 1);
+const lazyGrey = Color.fromRGBO(83, 82, 82, 1);
+const blackShadow = Color.fromRGBO(0, 0, 0, 0.16);
