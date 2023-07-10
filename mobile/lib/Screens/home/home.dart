871b32b62756308780fd9334e5c6176cb49c6992
Endy <endy.alexis@outlook.com>
2023-07-10 23:25:15 +0200

    Create home page component
    Added:
    - Project logo and assets
    - Inner home page component
    - Bottom navbar
    
    Updated:
    - Main file
    - Color constants
    - Home page component

--- mobile/lib/Screens/home/home.dart
+++ mobile/lib/Screens/home/home.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/Screens/home/components/navbar.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      bottomNavigationBar: BottomNavigationBarExample(),
+    );
   }
 }
