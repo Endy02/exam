d561a31eecb6c0b9beeaae10a368acb2b4e14de0
Endy <endy.alexis@outlook.com>
2023-07-11 10:02:09 +0200

    Create clean grid View :)
    Updated:
    - Order component view

--- mobile/lib/Screens/order/order.dart
+++ mobile/lib/Screens/order/order.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile/constants.dart';
 
 class Order extends StatefulWidget {
-  const Order({super.key});
+  const Order({Key? key}) : super(key: key);
 
   @override
   State<Order> createState() => _OrderState();
@@ -11,10 +11,11 @@ class Order extends StatefulWidget {
 class _OrderState extends State<Order> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(children: <Widget>[
-        Padding(
-            padding: const EdgeInsets.all(20),
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10),
             child: Container(
               height: 150,
               decoration: BoxDecoration(
@@ -22,15 +23,17 @@ class _OrderState extends State<Order> {
                 borderRadius: const BorderRadius.all(Radius.circular(20)),
               ),
               child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    SizedBox(
-                      child: Text("Yo man"),
-                    )
-                  ]),
-            )),
-        Padding(
-            padding: const EdgeInsets.all(20),
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  SizedBox(
+                    child: Text("Yo man"),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
             child: Container(
               height: 100,
               decoration: BoxDecoration(
@@ -38,54 +41,58 @@ class _OrderState extends State<Order> {
                 borderRadius: const BorderRadius.all(Radius.circular(20)),
               ),
               child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    SizedBox(
-                      child: Text("Yo man"),
-                    )
-                  ]),
-            )),
-        Container(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  SizedBox(
+                    child: Text("Yo man"),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
             child: GridView.count(
-          primary: false,
-          padding: const EdgeInsets.all(20),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          crossAxisCount: 2,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[100],
-              child: const Text("He'd have you all unravel at the"),
+              primary: false,
+              padding: const EdgeInsets.all(20),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.teal[100],
+                  child: const Text("He'd have you all unravel at the"),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.teal[200],
+                  child: const Text('Heed not the rabble'),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.teal[300],
+                  child: const Text('Sound of screams but the'),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.teal[400],
+                  child: const Text('Who scream'),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.teal[500],
+                  child: const Text('Revolution is coming...'),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.teal[600],
+                  child: const Text('Revolution, they...'),
+                ),
+              ],
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[200],
-              child: const Text('Heed not the rabble'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[300],
-              child: const Text('Sound of screams but the'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[400],
-              child: const Text('Who scream'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[500],
-              child: const Text('Revolution is coming...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[600],
-              child: const Text('Revolution, they...'),
-            ),
-          ],
-        ))
-      ]),
+          ),
+        ],
+      ),
     );
   }
 }
